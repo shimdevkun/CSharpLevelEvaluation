4ae77a5bc2d96d1d3533fd10acf02c4cee93616d
sshimabuku <shimasstrafez@gmail.com>
2020-11-19 22:24:45 -0500

    Implemented Register

---
+++ LevelEvaluation.Data/Person.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace LevelEvaluation.Data
+{
+    /// <summary>
+    /// This class represents a person's entity. It contains their name, age,
+    /// country and the languages they speak. The toString() method prints
+    /// how they would introduce themselves.
+    /// </summary>
+    public class Person
+    {
+        private string _name;
+        private int _age;
+        private string _country;
+        private string[] _languages;
+
+        // TODO: Implement a getter for the person's name
+        /// <summary>
+        /// This getter returns the person's name
+        /// </summary>
+
+        // TODO: Implement a getter for the person's age
+        /// <summary>
+        /// This getter returns the person's age
+        /// </summary>
+
+        // TODO: Implement a getter for the person's country
+        /// <summary>
+        /// This getter returns the person's country
+        /// </summary>
+
+
+        // TODO: Implement a constructor with a name as parameter
+        /// <summary>
+        /// This constructor takes a name and initializes
+        /// the other fields with the following data:
+        /// age: 25
+        /// country: "Canada"
+        /// languages: [ "English", "French" ]
+        /// </summary>
+
+        // TODO: Implement a constructor with parameters for each field
+        /// <summary>
+        /// This constructor takes data for all fields.
+        /// </summary>
+
+
+        /// <summary>
+        /// This method returns a string with all the languages spoken.
+        /// Ex.: languages = [ "French", "Spanish", "Italian", "Japanese" ]
+        /// Output: French, Spanish, Italian, Japanese
+        /// </summary>
+        private string ListLanguages()
+        {
+            // TODO: Implement the following method to match the description above
+            throw new NotImplementedException(); // comment this line when the implementation is done
+        }
+
+
+        public override string ToString()
+        {
+            return "Hey there! My name is " + this._name + ". I am " + this._age + 
+                    " years old. I speak the following languages: " + ListLanguages();
+        }
+    }
+}
--- Q2-Register/Program.cs
+++ Q2-Register/Program.cs
@@ -1,15 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using LevelEvaluation.Data;
+using System;
 
 namespace Q2_Register
 {
     class Program
     {
+        /// <summary>
+        /// This method prints info provided by a user about themselves.
+        /// The text after * should be displayed in the console.
+        /// The text surrounded by -- is an example of a user input.
+        /// You must complete the todos in the Person class first.
+        /// You must use the Person class with its ToString method.
+        /// </summary>
         static void Main(string[] args)
         {
+            Person person;
+
+            // * Hello, please enter your name:
+            // -- Max --
+
+            // * How old are you?
+            // -- 25 --
+
+            // * Where are you from?
+            // -- United States --
+
+            // * What languages do you speak?
+            // -- English French Japanese --
+
+            Console.WriteLine();
+
+            // * Hey there! My name is Max.
+            // * I am from the United States and I am 25 years old.
+            // * I speak the following languages: English, French, Japanese.
         }
     }
 }
